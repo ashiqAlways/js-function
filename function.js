4147a7f2fea2936c4237c6876b632cd502a96080
Ashik <ashikkhan757@gmail.coml>
2025-08-12 09:08:41 +0600

    added 3 new file

---
+++ function.js
@@ -0,0 +1,26 @@
+const price = 45;
+
+// function declare
+function fanOffKor(){
+    console.log('bosa theke uthe dara');
+    console.log('walk towards the switch');
+    console.log('click the switch to off of the Fan');
+}
+
+// call the function
+fanOffKor();
+
+
+
+// functon name(){
+// 
+// }
+
+function brashTeeth(){
+    console.log('pick up the brush');
+    console.log('add paste');
+    console.log('2 min ghoso and ghoso and fena felo');
+}
+
+// call the function
+brashTeeth();
---
+++ parameter.js
@@ -0,0 +1,47 @@
+function vaatKhao(){
+    console.log('hand wash kre aso');
+    console.log('boso');
+    console.log('plate a khabar dahbar nao');
+    console.log('ghopat ghopat kore khao');
+}
+
+// vaatKhao();
+
+
+// 3 --> 3*3 = 9
+// 4 --> 4*4 = 16
+// 5 --> 5*5 = 211
+// 11 --> 11*11 = 121
+
+
+function square(number){
+    console.log('value of the number parameter', number);
+    const borgo = number * number;
+    console.log('square of the given number is: ', borgo);
+
+}
+
+square(4);
+square(12);
+square(5);
+square(405);
+square(130);
+
+
+
+function add(num1, num2){
+    const sum = num1 + num2;
+    console.log(sum);
+}
+
+add(17, 52)
+add(65, 52)
+
+
+
+function addAll(a, b, c, d, e){
+    const total = a + b + c + d + e;
+    console.log(total);
+}
+
+addAll(3, 5, 12, 54,36)

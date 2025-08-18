625ca15ccb859dd1c71b90359c317543517985b5
Ashik <ashikkhan757@gmail.coml>
2025-08-19 05:36:25 +0600

    added some function files

--- function.js
+++ function.js
@@ -1,25 +1,23 @@
 const price = 45;
 
 // function declare
-function fanOffKor(){
-    console.log('bosa theke uthe dara');
-    console.log('walk towards the switch');
-    console.log('click the switch to off of the Fan');
+function fanOffKor() {
+  console.log("bosa theke uthe dara");
+  console.log("walk towards the switch");
+  console.log("click the switch to off of the Fan");
 }
 
 // call the function
 fanOffKor();
 
-
-
 // functon name(){
-// 
+//
 // }
 
-function brashTeeth(){
-    console.log('pick up the brush');
-    console.log('add paste');
-    console.log('2 min ghoso and ghoso and fena felo');
+function brashTeeth() {
+  console.log("pick up the brush");
+  console.log("add paste");
+  console.log("2 min ghoso and ghoso and fena felo");
 }
 
 // call the function
